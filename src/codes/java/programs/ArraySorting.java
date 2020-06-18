0cf19a7b00a7c4ef35284a5da24718ad24155b94
Ashish B <66672009+ashishb2codes@users.noreply.github.com>
2020-06-18 08:21:36 +0530

    Update ArraySorting

--- src/codes/java/programs/ArraySorting.java
+++ src/codes/java/programs/ArraySorting.java
@@ -5,6 +5,11 @@ public class ArraySorting {
 	public static void main(String[] args) {
 		
 		int[] arr = {4, 5, 2, 6, 1, 9, 7};
+		
+		System.out.println("Original Array: ");
+		for(int r = 0; r<arr.length; r++){
+			System.out.print(arr[r] + " ");
+		}
 
 	}
 
