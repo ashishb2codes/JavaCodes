c51c18ec2a434395774642d3f8985bb323afc492
Ashish B <ashish.bhure5@yahoo.in>
2020-06-18 08:15:28 +0530

    Changes in Array

--- src/codes/java/programs/ArraySorting.java
+++ src/codes/java/programs/ArraySorting.java
@@ -3,7 +3,8 @@ package codes.java.programs;
 public class ArraySorting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] arr = {4, 5, 2, 6, 1, 9, 7};
 
 	}
 
