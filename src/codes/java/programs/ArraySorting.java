4efd92ecc9c7f1a8427f6459e05ac374018ef1ed
Ashish B <ashish.bhure5@yahoo.in>
2020-06-18 09:47:14 +0530

    Array Sorted in Ascending Order

--- src/codes/java/programs/ArraySorting.java
+++ src/codes/java/programs/ArraySorting.java
@@ -3,26 +3,30 @@ package codes.java.programs;
 public class ArraySorting {
 
 	public static void main(String[] args) {
-		
+
 		int tempVar;
 		int[] arr = {4, 5, 2, 6, 1, 9, 7};
-		
+
 		System.out.println("Original Array: ");
 		for(int r = 0; r<arr.length; r++){
 			System.out.print(arr[r] + " ");
 		}
-		
+
 		System.out.println();
 		System.out.println("Array in Ascending Order: ");
 		for(int r = 0; r<arr.length; r++){
-			if(arr[r]>arr[r+1]){
-				tempVar = arr[r];
-				arr[r] = arr[r+1];
-				arr[r+1] = tempVar;
+			for(int c=r+1; c<arr.length; c++) {
+				if(arr[r]>arr[c]){
+					tempVar = arr[r];
+					arr[r] = arr[c];
+					arr[c] = tempVar;
+				}
 			}
 		}
-		
-		
+
+		for(int r=0; r<arr.length; r++) {
+			System.out.print(arr[r] +"  ");
+		}
 
 	}
 
